b12fc364bf5254ebb550e2d8a8ce8ea483898f98
Ghost <michal.miladowski@gmail.com>
2022-01-27 14:37:30 +0100

    Changed unit test logging logic

---
+++ .github/workflows-scripts/clean-unitlogs.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+actual_date=$(date +%s)
+
+for file in unitlogs/*.log
+do
+    file_date=$(basename $file .log)
+    difference=$((actual_date - file_date))
+
+    if [ $difference -ge 86400 ]
+    then
+        rm $file
+    fi
+done
