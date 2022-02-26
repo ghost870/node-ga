8c82fc44385d513e2b4bb521b13acbf0d26172b4
ghost870 <84833594+ghost870@users.noreply.github.com>
2022-02-26 11:43:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ const server = http.createServer((req, res) => {
     res.end('OK...')
 })
 
-server.listen(process.env.PORT || 3000, () => {})
+server.listen(process.env.PORT || 3000, () => {
+    console.log('Ready...')
+})
